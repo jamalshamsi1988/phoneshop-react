23caadc639031afb43bd624eb61ea50a641f99c6
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-01-04 05:34:10 -0800

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
           <li><Link to="/products">Products</Link> </li>
           <li><Link to="/products/aboutus">About Us</Link></li>
           <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/login">Login</Link></li>
+          {/* <li><Link to="/login">Login</Link></li> */}
 
         </ul>
       </div>
